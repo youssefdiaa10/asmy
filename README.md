bacd7f7cf321b1c0f25c1c8d45779c6d17c0d9f7
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2024-12-13 01:39:30 +0200

    Establish Asmy

---
+++ README.md
@@ -0,0 +1,3 @@
+# Say Functions
+
+Enter your name and return it in a sentence
---
+++ src/functions.ts
@@ -0,0 +1,11 @@
+export function greet(name: string): string {
+  return `Hi, I am ${name}`;
+}
+
+export function motivate(name: string): string {
+  return `You can do it ${name}. I believe in you.`;
+}
+
+export function love(name: string): string {
+  return `I love ${name} 💘.`;
+}
